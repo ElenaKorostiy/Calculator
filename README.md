3b42d9b0d9663a716334bc55f0c9d5ba70c29a13
ElenaKorostiy <go.lena.83@gmail.com>
2023-10-28 17:49:09 +0300

    one

---
+++ README.md
@@ -0,0 +1,10 @@
+# Calculator 
+ Here is a simple calculator that helps you make various calculations.
+
+# Technologies
+1.HTML
+2.CSS
+3.JavaScript
+
+# DEMO 
+[demo]( )
---
+++ js.js
@@ -0,0 +1,63 @@
+const screenDisplay = document.querySelector(`.screen`)
+const buttons = document.querySelectorAll(`button`)
+
+
+let calculation = [];
+let accumulativeCalculation = " ";
+
+function calculate(button) {
+    const value = button.textContent;
+    if (value === "C") {
+        calculation = [];
+        accumulativeCalculation = " ";
+        screenDisplay.textContent = '0';
+    } else if (value === "=") {
+        try {
+            const result = eval(accumulativeCalculation);
+            screenDisplay.textContent = result;
+            accumulativeCalculation = result.toString();
+            calculation = [result.toString()];
+        } catch (error) {
+            screenDisplay.textContent = 'Error';
+        }
+    } else if (value === "%") {
+        if (calculation.length >= 1) {
+            const lastValue = parseFloat(accumulativeCalculation);
+            const percentValue = lastValue / 100;
+            accumulativeCalculation = percentValue.toString();
+            calculation = [percentValue.toString()];
+            screenDisplay.textContent = accumulativeCalculation;
+        }
+    } else {
+        if (!isNaN(value) || value === "+" || value === "-" || value === "*" || value === "/" || (value === "." && !accumulativeCalculation.endsWith("."))) {
+            calculation.push(value);
+            accumulativeCalculation = calculation.join('');
+            screenDisplay.textContent = accumulativeCalculation;
+        }
+    }
+}
+
+document.getElementById('back').addEventListener('click', function () {
+    if (accumulativeCalculation.length > 0) {
+        accumulativeCalculation = accumulativeCalculation.slice(0, -1);
+        calculation = [accumulativeCalculation];
+        screenDisplay.textContent = accumulativeCalculation;
+    }
+});
+
+function buttonClickHandler(event) {
+    const button = event.target;
+    calculate(button);
+}
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', buttonClickHandler);
+});
+
+
+
+
+
+
+
+
